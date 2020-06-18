601e43865bef51e777a3d01969ce90f92a0c33e2
Nicholas J. Fiorello Jr <nfiorello@modusoperandi.com>
2020-06-18 09:46:10 -0400

    save / load / sample docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { HELLO_WORLD } from './hello-world';
 import { cloneDeep } from 'lodash';
+import { saveAs } from 'file-saver';
+
+import HELLO_WORLD from './docs/hello-world.json';
+import FONT_TEST from './docs/font-test.json';
 
 @Component({
   selector: 'app-root',
@@ -24,5 +27,38 @@ export class AppComponent {
 
   hello() {
     this.content = cloneDeep(HELLO_WORLD);
+  }
+
+  fonts() {
+    this.content = cloneDeep(FONT_TEST);
+  }
+
+  save() {
+    const json = JSON.stringify(this.content, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    saveAs(blob, 'content.json');
+  }
+
+  private read(file: File): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        try {
+          resolve(JSON.parse(event.target.result as string));
+        } catch {
+          reject(new Error("Unable to parse file"));
+        }
+      }
+      reader.onerror = () => {
+        reject(new Error("Error reading file"));
+      }
+      reader.readAsText(file, 'utf-8');
+    });
+  }
+
+  async load(files: ArrayLike<File>) {
+    if (files.length) {
+      this.content = await this.read(files[0]);
+    }
   }
 }
