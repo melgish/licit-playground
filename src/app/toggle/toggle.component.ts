a673129f0284d26d4c2649db994900499eeee610
Nicholas J. Fiorello Jr <nfiorello@modusoperandi.com>
2020-06-18 10:35:43 -0400

    Indicate what is enabled/disabled

---
+++ src/app/toggle/toggle.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, forwardRef, HostBinding, HostListener, ViewEncapsulation } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { noop } from 'rxjs';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => ToggleComponent),
+    multi: true
+  }]
+})
+export class ToggleComponent implements ControlValueAccessor {
+  /**
+   * Holds input value.
+   */
+  @HostBinding('class.on')
+  value: boolean = false;
+  /**
+   * Angular supplied change notification method.
+   */
+  private onChange: (value: boolean) => void = noop;
+  /**
+   * Angular supplied touch notification method.
+   */
+  private onTouched: () => void = noop;
+  /**
+   * Update display when external value is changed.
+   * @param value value to display
+   */
+  writeValue(value: boolean): void {
+    this.value = Boolean(value);
+  }
+  /**
+   * Called by angular to register change detection callback.
+   * @param fn
+   */
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+  /**
+   * Called by angular to register touch detection callback.
+   * @param fn
+   */
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Event handler fired when button is clicked.
+   */
+  click() {
+    this.value = !this.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
+}
